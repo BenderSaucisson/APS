e63f5ec054ddc90a0c257c65b0b328c805ced8b4
BenderSaucisson <bender.saucisson@gmail.com>
2020-10-23 11:47:53 +0200

    migration de la fonction d'ajustement du timestamp de getTime a simulateur

--- Python/simulateur.py
+++ Python/simulateur.py
@@ -1,12 +1,4 @@
 import getTime
 import pandas
 
-def erreurTemps():
-  tempsSec = getTime.get_start_time_simulateur_s()
-  #incrémentation de deux sur le temps en UNIX car le temps que nous donne le log n'est pas fidèle au csv
-  tempsSec += 2
-  #décrémentation de 67 car l'heure du pc sur lequel on a fait les tests n'était pas exactement à l'heure
-  tempsSec -= 67
-  return(tempsSec)
-
-def ajustement():
+#def ajustement():
